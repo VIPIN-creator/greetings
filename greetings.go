9973799c3e43a75c7ef31e7784a5505e3d80296e
Vipin Kumar Tripathi <vipinkumartripathi@Vipins-MacBook-Air.local>
2025-12-04 21:59:19 +0530

    corrected the Hellos signature

--- greetings.go
+++ greetings.go
@@ -6,7 +6,7 @@ import (
     "math/rand"
 )
 
-func Hellos(names []string) (map[string]string, err) {
+func Hellos(names []string) (map[string]string) {
     messages := make(map[string]string)
 
     //In this for loop, range returns two values: the index of the current item in the loop and a copy of the item's value. 
